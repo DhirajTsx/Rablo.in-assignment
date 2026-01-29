6064025ee0dabb5eee1dd146858f2be0e67b966e
DhirajTsx <dhirajbhavsar.official@gmail.com>
2026-01-30 01:18:41 +0530

    middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,17 +5,17 @@ export function middleware(req: NextRequest) {
   const user = req.cookies.get("userID")?.value;
   const acc = req.cookies.get("accCreated")?.value;
 
-  // if (!user && req.nextUrl.pathname !== "/login") {
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
+  if (!user && req.nextUrl.pathname !== "/login") {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
-  // if (user && acc === "0" && req.nextUrl.pathname !== "/create-profile") {
-  //   return NextResponse.redirect(new URL("/create-profile", req.url));
-  // }
+  if (user && acc === "0" && req.nextUrl.pathname !== "/create-profile") {
+    return NextResponse.redirect(new URL("/create-profile", req.url));
+  }
 
-  // if (user && acc === "1" && req.nextUrl.pathname !== "/dashboard") {
-  //   return NextResponse.redirect(new URL("/dashboard", req.url));
-  // }
+  if (user && acc === "1" && req.nextUrl.pathname !== "/dashboard") {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
 
   return NextResponse.next();
 }
